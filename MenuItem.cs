504a160ab956a767536d433e26943134941f6a9a
Christian Doyle <doyaldinho@gmail.com>
2021-05-09 14:57:48 +0100

    Add project files.

---
+++ MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SloppyJoe_sApp
+{
+    class MenuItem
+    {
+        public Random randomizer = new Random();
+
+        public string[] proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
+
+        public string description = "";
+        public string price;
+    }
+}
