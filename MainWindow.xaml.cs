66efd5ca909e46430691ee9608a40ba31afbbd52
Christian Doyle <doyaldinho@gmail.com>
2021-05-09 15:33:43 +0100

    Added functionality to random menu.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,54 @@ namespace SloppyJoe_sApp
         public MainWindow()
         {
             InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            string guacamolePrice;
+
+            for (int i = 0; i < 5; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].breads = new string[] { "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel" };
+                }
+                menuItems[i].Generate();
+            }
+
+            item1.Text = menuItems[0].description;
+            price1.Text = menuItems[0].price;
+            item2.Text = menuItems[1].description;
+            price2.Text = menuItems[1].price;
+            item3.Text = menuItems[2].description;
+            price3.Text = menuItems[2].price;
+            item4.Text = menuItems[3].description;
+            price4.Text = menuItems[3].price;
+            item5.Text = menuItems[4].description;
+            price5.Text = menuItems[4].price;
+
+            MenuItem specialMenuItem = new MenuItem()
+            {
+                proteins = new string[] { "Organic ham", "Mushroom patty", "Mortadella" },
+                breads = new string[] { "a gluten free roll", "a wrap", "pita" },
+                condiments = new string[] { "dijon mustard", "miso dressing", "au jus" }
+            };
+            specialMenuItem.Generate();
+
+            item6.Text = specialMenuItem.description;
+            price6.Text = specialMenuItem.price;
+
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamolePrice = guacamoleMenuItem.price;
+
+            guacamole.Text = "Add guacamole for " + guacamoleMenuItem.price;
+
+
+
         }
     }
 }
--- MenuItem.cs
+++ MenuItem.cs
@@ -6,13 +6,27 @@ namespace SloppyJoe_sApp
 {
     class MenuItem
     {
-        public Random randomizer = new Random();
+        public static Random randomizer = new Random();
 
         public string[] proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
-        public string[] condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] condiments = { "english mustard", "american mustard", "ketchup", "french mustard", "honey mustard", "mayo", "perri perri mayo" ,"relish", "french dressing" };
         public string[] breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
 
         public string description = "";
         public string price;
+
+        public void Generate()
+        {
+            string randomProtein = proteins[randomizer.Next(proteins.Length)];
+            string randomCondiment = condiments[randomizer.Next(condiments.Length)];
+            string randomBread = breads[randomizer.Next(breads.Length)];
+            description = randomProtein + " with " + randomCondiment + " on " + randomBread;
+
+            decimal pounds = randomizer.Next(2, 5);
+            decimal pence = randomizer.Next(1, 98);
+            decimal cost = pounds + (pence * .01M);
+            price = cost.ToString("c");
+        }
+
     }
 }
